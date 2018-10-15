0829fd34de80ebe2861ef4103e21febab0ac3445
Patrick E. Leser <patrick.e.leser@nasa.gov>
2018-10-15 16:27:13 -0400

    Added ability to plot ground truth on pdf plots. Also renamed plotting file and updated imports in the sampler.

--- mcmcpy/mcmc/mcmc_sampler.py
+++ mcmcpy/mcmc/mcmc_sampler.py
@@ -9,7 +9,7 @@ from ..step_methods.step_methods import DelayedRejectionAdaptiveMetropolis
 from ..step_methods.step_methods import SMC_Metropolis
 from scipy.optimize import minimize
 from scipy.stats import gaussian_kde
-from ..plotting.MCMCplots import time_vs_observations, pdf, pairwise, residuals
+from ..plotting.mcmc_plots import time_vs_observations, pdf, pairwise, residuals
 
 
 class MCMCSampler:
